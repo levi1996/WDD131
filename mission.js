c6eaf22c2c6edd93e119c181814c66e21c70750b
levi <levi1996>
2024-10-02 00:46:47 -0600

    move

--- mission.js
+++ mission.js
@@ -4,15 +4,15 @@ const logo = document.querySelector('.logo');
 
 
 function changeTheme() {
-    // Check the selected value
+    
     if (themeSelector.value === 'dark') {
         document.body.classList.add('dark');
-        logo.src = 'content/byui-logo_blue.webp'; // Update this with the correct path to your white logo
+        logo.src = 'content/byui-logo_blue.webp'; 
     } else {
         document.body.classList.remove('dark');
-        logo.src = 'content/byui-logo_blue.webp'; // Original logo
+        logo.src = 'content/byui-logo_blue.webp'; 
     }
 }
 
-// Add event listener to the select element
+
 themeSelector.addEventListener('change', changeTheme);
