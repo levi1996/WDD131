32b73e92bc23efc0287071658493e9191949af3c
levi <levi1996>
2024-10-05 19:31:12 -0600

    cool pics

---
+++ coolpics/coolpics.js
@@ -0,0 +1,9 @@
+
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+});
